54cac5fef1ed54ddde10779b496df747df4ef4c4
Yesid <yellaber@gmail.com>
2026-01-17 13:30:29 -0500

    feat(companies): estructura inicial del módulo companies

---
+++ src/companies/dto/update-company.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCompanyDto } from './create-company.dto';
+
+export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {
+  slug?: string;
+  status?: string;
+  updateAt: Date;
+}
