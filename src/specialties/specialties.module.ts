0f6827473d53067c10321cdadfcd4676ad711433
Yesid <133909477+Yellaber@users.noreply.github.com>
2026-02-06 14:07:59 -0500

    chore(specialties): aplicar correcciones menores tras revisión (#9)

--- src/specialties/specialties.module.ts
+++ src/specialties/specialties.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { CompaniesModule } from 'src/companies/companies.module';
+import { Specialty } from './entities/specialty.entity';
 import { SpecialtiesService } from './specialties.service';
 import { SpecialtiesController } from './specialties.controller';
-import { Specialty } from './entities/specialty.entity';
 
 @Module({
   controllers: [ SpecialtiesController ],
