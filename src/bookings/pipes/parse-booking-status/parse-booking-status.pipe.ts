5a4bdbdfe46a49a2088db5a75eb00b889e29a2cd
Yesid <yellaber@gmail.com>
2026-04-01 12:36:47 -0500

    refactor(bookings): mejorar implementación de ParseBookingStatusPipe

--- src/bookings/pipes/parse-booking-status/parse-booking-status.pipe.ts
+++ src/bookings/pipes/parse-booking-status/parse-booking-status.pipe.ts
@@ -1,14 +1,16 @@
 import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { AppointmentStatus } from 'src/common/enums';
 
-const allowedStatuses = [ AppointmentStatus.CONFIRMED, AppointmentStatus.CANCELED, AppointmentStatus.COMPLETED ];
+const allowedStatuses = Object.values(AppointmentStatus);
 
 @Injectable()
 export class ParseBookingStatusPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata) {
-    if(!allowedStatuses.includes(value as AppointmentStatus))
-      throw new BadRequestException(`Invalid booking status: ${ value }`);
+    const status = value.toLowerCase() as AppointmentStatus;
 
-    return value;
+    if(!allowedStatuses.includes(status))
+      throw new BadRequestException(`Invalid booking status: "${ value }". Allowed values are: ${ allowedStatuses.join(', ') }`);
+
+    return status;
   }
 }
