2d11c67d34fda953c81b6d328ac027308e2a8c08
Yesid <yellaber@gmail.com>
2026-04-04 02:34:20 -0500

    refactor(specialists): centralizar construcción de respuestas en SpecialistResponse helper

---
+++ src/specialists/helpers/specialist-response.helper.ts
@@ -0,0 +1,21 @@
+import { User } from 'src/users/entities/user.entity';
+import { DataUserResponseDto, PaginationSpecialistResponseDto, SpecialistResponseDto } from '../dto';
+import { Specialist } from '../entities/specialist.entity';
+
+export class SpecialistResponse {
+    static getDataUser(authenticatedUser: User): DataUserResponseDto {
+        const { id, fullName, image } = authenticatedUser;
+        return { id, fullName, image };
+    }
+
+    static get(specialist: Specialist): SpecialistResponseDto {
+        const { id, user } = specialist;
+        const dataUser = this.getDataUser(user);
+        return { id, user: dataUser };
+    }
+
+    static getPagination(total: number, specialists: Specialist[]): PaginationSpecialistResponseDto {
+        const specialistsResponse = specialists.map(specialist => this.get(specialist));
+        return { total, specialists: specialistsResponse };
+    }
+}
