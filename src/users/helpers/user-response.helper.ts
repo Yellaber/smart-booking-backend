446bd4f38d1afa66dc787a2f7013f8e7b142f62d
Yesid <yellaber@gmail.com>
2026-04-04 02:43:27 -0500

    refactor(users): centralizar construcción de respuestas en UserResponse helper

---
+++ src/users/helpers/user-response.helper.ts
@@ -0,0 +1,14 @@
+import { PaginationUserResponseDto, UserResponseDto } from '../dto';
+import { User } from '../entities/user.entity';
+
+export class UserResponse {
+    static get(user: User): UserResponseDto {
+        const { password, company, bookings, isActive, ...userResponse } = user;
+        return userResponse;
+    }
+
+    static getPaginationUser(total: number, users: User[]): PaginationUserResponseDto {
+        const usersResponse = users.map(user => this.get(user));
+        return { total, users: usersResponse };
+    }
+}
